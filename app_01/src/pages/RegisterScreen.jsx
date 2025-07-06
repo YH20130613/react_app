b9ae28b5ef940e5d63ae706cd19d91f01257cdcb
local_45 <yoko.79454542@gmail.com>
2025-07-07 01:19:48 +0900

    その他画面のベース構築

---
+++ app_01/src/pages/RegisterScreen.jsx
@@ -0,0 +1,14 @@
+// src/pages/RegisterScreen.jsx
+import React from 'react'
+
+
+const RegisterScreen = () => {
+  return (
+    <div>
+      <div>RegisterScreen</div>
+
+    </div>
+  );
+};
+
+export default RegisterScreen
---
+++ app_01/src/pages/WordListScreen.jsx
@@ -0,0 +1,14 @@
+// src/pages/WordListScreen.jsx
+import React from 'react'
+
+
+const WordListScreen = () => {
+  return (
+    <div>
+      <div>WordListScreen</div>
+
+    </div>
+  );
+};
+
+export default WordListScreen
