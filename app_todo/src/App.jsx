1ebf73f71636de8d4954a0d10faf922aae4fc49d
local_45 <yoko.79454542@gmail.com>
2025-07-13 21:35:40 +0900

    stepの表示順を降順に

--- app_todo/src/App.jsx
+++ app_todo/src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css'
 import React, { useState } from 'react';
-import Step1 from './pages/Step1';
-import Step2 from './pages/Step2';
-import Step3 from './pages/Step3';
-import Step4 from './pages/Step4';
-import Step5 from './pages/Step5';
 import Step6 from './pages/Step6';
+import Step5 from './pages/Step5';
+import Step4 from './pages/Step4';
+import Step3 from './pages/Step3';
+import Step2 from './pages/Step2';
+import Step1 from './pages/Step1';
 import Pagination from './components/Pagination';
 
 function App() {
-  const [currentStep, setCurrentStep] = useState(1); // 現在のステップを管理
+  const [currentStep, setCurrentStep] = useState(6); // 現在のステップを管理
   const totalSteps = 6; // 全ステップ数
   const goToNextStep = () => {
     if (currentStep < totalSteps) {
@@ -24,18 +24,18 @@ function App() {
   // 現在のステップに基づいて表示するコンポーネントを決定
   const renderStep = () => {
     switch (currentStep) {
-      case 1:
-        return <Step1 />;
-      case 2:
-        return <Step2 />;
-      case 3:
-        return <Step3 />;
-      case 4:
-        return <Step4 />;
-      case 5:
-        return <Step5 />;
       case 6:
         return <Step6 />;
+      case 5:
+        return <Step5 />;
+      case 4:
+        return <Step4 />;
+      case 3:
+        return <Step3 />;
+      case 2:
+        return <Step2 />;
+      case 1:
+        return <Step1 />;
       default:
         return <p className="text-red-500">エラー: 不明なステップです。</p>;
     }
